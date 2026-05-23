aa47c84c640770a9d444e78ed89f491b1d1ac2a9
Aashif-Malikk <aashifmalik776@gmail.com>
2026-05-23 21:28:51 +0530

    removed both folders

--- Frontend/src/Component/auth/RealAuth.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-
-function RealAuth() {
-    const isLoggedin = localStorage.getItem('isLoggedIn')
-    return isLoggedin ? <Outlet /> : <Navigate to='/auth/signup' />
-}
-
-export default RealAuth
